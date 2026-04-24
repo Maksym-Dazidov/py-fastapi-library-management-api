7c5cb2e8efd0b375db0d443dd5a52136925d4891
Maksym Dazidov <maksymdazidov@gmail.com>
2026-04-24 17:31:49 +0200

    3

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
+import models
 import schemas
-from database import SessionLocal
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 def get_db():
     db = SessionLocal()
@@ -14,13 +18,16 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/authors/", response_model=list[schemas.Author])
 def get_authors(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_authors(db, skip=skip, limit=limit)
 
+
 @app.post("/authors/", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db, author=author)
+
 
 @app.get("/authors/{author_id}", response_model=schemas.Author)
 def get_author(author_id: int, db: Session = Depends(get_db)):
@@ -29,6 +36,7 @@ def get_author(author_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Author not found")
     return author
 
+
 @app.post("/authors/{author_id}/books", response_model=schemas.Book)
 def create_book(author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
     author = crud.get_author(db, author_id=author_id)
@@ -36,10 +44,12 @@ def create_book(author_id: int, book: schemas.BookCreate, db: Session = Depends(
         raise HTTPException(status_code=404, detail="Author not found")
     return crud.create_book(db, book=book, author_id=author_id)
 
+
 @app.get("/books/", response_model=list[schemas.Book])
 def get_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_books(db, skip=skip, limit=limit)
 
+
 @app.get("/books/by-author/{author_id}", response_model=list[schemas.Book])
 def get_books_by_author(author_id: int, skip: int, limit: int, db: Session = Depends(get_db)):
-    author = crud.get_books_by_author(db, author_id=author_id, skip=skip, limit=limit)
+    return crud.get_books_by_author(db, author_id=author_id, skip=skip, limit=limit)
